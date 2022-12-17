65174dca4c13380ad31de3b91bbaad82f9043ab8
kennezes <62331014+kennezes@users.noreply.github.com>
2022-12-18 00:41:34 +0800

    Add FoodOrder main function.

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,7 +2,7 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -15,4 +15,14 @@ public class FoodOrderGUI {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+
+    public static void main(String[] args) {
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setTitle("Food Ordering System");
+        app.setContentPane(app.panel1);
+        app.setSize(1300, 800);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
