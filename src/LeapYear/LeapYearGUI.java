467fd1d6cf4d284c0ef8a9ccadeeeb8f9a5cc8f1
kennezes <62331014+kennezes@users.noreply.github.com>
2022-12-17 23:08:52 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,12 +1,31 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    String IYear = tfYear.getText();
+                    int year = Integer.parseInt(IYear);
+                    if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)){
+                        JOptionPane.showMessageDialog(panel1, "Leap Year");
+                    }
+                    if (year < 0) {
+                        throw new IllegalArgumentException("Years not be negative");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(panel1, "Not a Leap Year");
+                    }
 
+            }
+        });
+    }
     public static void main(String[] args) {
         LeapYearGUI Zane = new LeapYearGUI();
         Zane.setTitle("Leap Year Checker");
