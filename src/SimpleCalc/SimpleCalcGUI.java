6ca04c44a3efadfe105c7e39450a960b12b26780
kennezes <62331014+kennezes@users.noreply.github.com>
2022-12-17 23:39:41 +0800

    Complete Simple Calculator.

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -12,6 +12,38 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+    public SimpleCalcGUI(){
+
+        btnCompute.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    String input1 = tfNumber1.getText();
+                    int inputNum1 = Integer.parseInt(input1);
+
+                    String input2 = tfNumber2.getText();
+                    int inputNum2 = Integer.parseInt(input2);
+                    int equation = 0;
+                    String operator = (String) cbOperations.getSelectedItem();
+
+
+                    switch (operator) {
+                        case "+":
+                            equation = inputNum1 + inputNum2;
+                            break;
+                        case "-":
+                            equation = inputNum1 - inputNum2;
+                            break;
+                        case "*":
+                            equation = inputNum1 * inputNum2;
+                            break;
+                        case "/":
+                            equation = inputNum1 / inputNum2;
+                            break;
+                    }
+                    lblResult.setText(Integer.toString(equation));
+
+            }
+        });
+    }
     public static void main(String[] args) {
 
         SimpleCalcGUI Zane = new SimpleCalcGUI();
